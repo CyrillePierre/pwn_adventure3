b84ad300d9075b151517c9d72f6d20fd7d81c59f
Cyrille <cypierre58@gmail.com>
2019-11-01 23:57:03 +0100

    [proxy] Parse chat, make, select_item

--- server_proxy/parser.py
+++ server_proxy/parser.py
@@ -4,17 +4,21 @@ import binascii
 jump_state = 0
 walk_state = 0
 
-def raw(data):
-    s = '{} raw['.format(data[:2].decode())
-#    s += str(data)
-    for b in data[2:]:
+
+def raw_str(data):
+    s = 'raw['
+    for b in data:
         s += '{:02x} '.format(b)
-    s = s[:-1] + ']'
+    return s[:-1] + ']'
+
+
+def unknown(data):
+    s = '{} {}'.format(data[:2], data[2:])
     return s, b''
 
 
-def move(data, offset=2):
-    x,y,z, hp,hy,hr, mx,my = struct.unpack_from('fffhhhbb', data, offset)
+def move(data):
+    x,y,z, hp,hy,hr, mx,my = struct.unpack_from('<3f3h2b', data, 2)
 
     # movement buttons
     bx = ['b', ' ', 'f'][int(mx/127) + 1]
@@ -26,7 +30,7 @@ def move(data, offset=2):
     pitch = hp * 180. / 32768
     yaw = hy * 180. / 32768
 
-    s = '{} pos[{:6.0f}{:6.0f}{:6.0f}]  rpy[{:7.1f}{:7.1f}{:7.1f}]  btn[{}{}{}{}]'.format(
+    s = '{} pos[{:7.0f}{:7.0f}{:7.0f}]  rpy[{:7.1f}{:7.1f}{:7.1f}]  btn[{}{}{}{}]'.format(
             data[:2].decode(), x,y,z, roll,pitch,yaw, bx,by,bj,bw)
     return s, data[22:]
 
@@ -43,14 +47,55 @@ def run(data):
     return '', data[3:]
 
 
+def make(data):
+    ind, _, str_len = struct.unpack_from('<QbH', data, 2)
+    str_start = 13
+    str_end = str_start + str_len
+    name = data[str_start:str_end].decode()
+    x,y,z = struct.unpack_from('<3f', data, str_end)
+
+    s = '{}({:02d}) {:20s}[{:7.0f}{:7.0f}{:7.0f}] {}'.format(
+        data[:2].decode(), ind, name, x,y,z, raw_str(data[str_end+12:str_end+22]))
+    return s, data[str_end+22:]
+
+
+def chat(data, name):
+    if name[0] == 'c':
+        msg_len, = struct.unpack_from('<H', data, 2)
+        msg = data[4:4+msg_len].decode()
+        s = '{} send: "{}"'.format(data[:2].decode(), msg)
+        return s, data[4+msg_len:]
+
+    else:
+        nb, msg_len = struct.unpack_from('<IH', data, 2)
+        msg = data[8:8+msg_len].decode()
+        s = '{} chat: [{}] "{}"'.format(data[:2].decode(), nb, msg)
+        return s, data[8+msg_len:]
+
+
+def select_item(data, name):
+    if name[0] == 'c':
+        nb, = struct.unpack_from('<B', data, 2)
+        s = '{} select item[{}]'.format(data[:2].decode(), nb)
+        return s, data[3:]
+
+    else:
+        nb, = struct.unpack_from('<B', data, 2)
+        s = '{} select item[{}]  {}'.format(data[:2].decode(), nb, raw_str(data[3:5]))
+        return s, data[5:]
+
+
 def parse(name, data):
     cases = { 
         b'\0\0': (0, lambda x: ('ok', b'')),
-        b'mv': (1, move),
+        b'mv': (0, move),
         b'jp': (0, jump),
         b'rn': (0, run),
+        b'mk': (1, make),
+        b'#*': (1, lambda x: chat(x, name)),
+        b's=': (1, lambda x: select_item(x, name)),
     }
-    default = (0, raw)
+    default = (1, unknown)
     
     msgs = []
     while len(data):
@@ -59,4 +104,7 @@ def parse(name, data):
         if enabled: msgs.append(s)
 
     if len(msgs):
-        print('[{}] '.format(name).join(msgs))
+        prefix = '[{}] '.format(name)
+        sep = '\n' + prefix
+        print(prefix, end='')
+        print(sep.join(msgs))
